b42a7b40d1e0e6e7467eb26e0c2be4d36f934234
Allada Venkateshwar Rao <venkateshwar2929@gmail.com>
2025-09-16 11:11:29 +0000

    second commit

---
+++ definitions/sources.js
@@ -0,0 +1,22 @@
+// definitions/sources.js
+
+// Sales data source
+declare({
+  database: "project-96b123a3-d2f8-4312-b92",
+  schema: "raw",
+  name: "sales_data"
+});
+
+// Google Ads source
+declare({
+  database: "project-96b123a3-d2f8-4312-b92",
+  schema: "raw",
+  name: "google_ads"
+});
+
+// Facebook Ads source
+declare({
+  database: "project-96b123a3-d2f8-4312-b92",
+  schema: "raw",
+  name: "facebook_ads"
+});
